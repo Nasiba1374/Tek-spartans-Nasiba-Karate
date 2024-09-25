fa7b451014cad63fcf7c8753077a237199ec5081
nasibamehraban <nasibamehraban@gmail.com>
2024-09-24 21:52:21 -0400

    cucumber jason

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ public class TestRunner {
         return Karate
                 .run("classpath:features")
                 .karateEnv("dev")
-                .tags("@End2EndAccount")
+                //.tags("@End2EndAccount")
                 .outputCucumberJson(true);
 
     }
